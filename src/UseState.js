b507f2f46bcf0e992c9cd9fcb7fb6381d7dcbacc
mrodas121 <manuel.rodas121@gmail.com>
2021-12-06 21:21:02 -0600

    cambios pendientes

---
+++ src/UseState.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UseState = () =>{
+    return(
+        <div>
+            <h2>Eliminar UseState</h2>
+            <p>Por favor, escribe el codigo de seguridad.</p>
+            <input placeholder="Código de seguridad"/>
+            <button>Comprobar</button>
+        </div>
+    );
+}
+
+export {UseState};
